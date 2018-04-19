1f299faf9b55e0e734d3638806c9d59452f0a514
Irene <iychiangsd@gmail.com>
2018-04-19 11:12:28 -0700

    Refactoring + placeholder picture

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Astro Weight Calculator
+__Astro Weight Calculator__
+
+This nifty calculator will show you all the weighs to prepare yourself for traversing the outer space! Want to plan for your trip to Pluto? Find out how much you weigh before you pack up for the last adventure of your lifetime! Or maybe you're one of those heartless people who don't think Pluto is a planet anymore-- that's alright too! Simply select the "Hide Pluto" option before you ever have to see that pesky planet again.
+
+*Author's note:*
+*Easter egg included!*
--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var planets = [
     ['Earth', 1],
     ['Venus', 0.9032],
     ['Mercury', 0.377],
-    ['Sun', 27.9]
+    ['The Sun', 27.9]
 ];
 
 
@@ -21,14 +21,10 @@ let checkBox = document.getElementById("pluto");
 let clear = document.getElementById("clear-button");
 let outputText = document.getElementById("output");
 var plutoID = planets[0][0]; //grabs 'Pluto'
-var sunID = planets[10];
-
 
 function populateOptions() {
-        //let index = 0;
-        planets.reverse().forEach(function ([planetName]) {
+    planets.reverse().forEach(function ([planetName]) {
         let optionElm = document.createElement("option");
-        optionElm.value = planetName;
         optionElm.innerText = planetName;
         planetsElm.appendChild(optionElm);
     });
@@ -37,83 +33,64 @@ function populateOptions() {
 populateOptions();
 
 function calculateWeight(userWeight, selectedPlanet) {
-
-    // var userWeight = document.getElementById("user-weight").value;
-    // var selectedPlanet = planetsElm.options[planetsElm.selectedIndex].value;
-    for (var i=0; i < planets.length;i++) {
+    for (var i = 0; i < planets.length; i++) {
         var planetInfo = planets[i];
-            // for (i=0; i < planets.length; i++) {
-            //     if (selectedPlanet === planets[i][0]) {
-            //    console.log(planets[i][1]);
-            // } 
+
         if (planetInfo[0] === selectedPlanet) {
             var weightFactor = planetInfo[1];
-        }
-        
-        if (selectedPlanet === "Sun") {
-            console.log("SUNTEXT")
-            outputText.innerHTML = "(And you'd be dead)";
-        }
-        
-
+        };
     }
 
-    var result = userWeight * weightFactor;
-    outputText.innerHTML = "If you were on " + selectedPlanet + ", you would weigh " + result + "lbs!";
-
+    var result = (userWeight * weightFactor);
+    outputText.innerHTML = "If you were on " + selectedPlanet + ", you would weigh " + result.toLocaleString() + " lbs!";
+    
+    if (selectedPlanet === planets[0][0]) { //planets[0] is Sun due to .reverse
+        outputText.innerHTML = "If you were on the Sun, you'd be " + result.toLocaleString() + " lbs but you'd also be dead!";
+    };
 }
 
 function hidePluto() {
     if (document.getElementById("pluto").checked === true) {
-        planetsElm.lastChild.style.display="none";
+        planetsElm.lastChild.style.display = "none";
         // works planetsElm.childNodes[10].style.display="none";
-        //test 1 planetsElm.removeChild(planetsElm.childNodes[0][0]);
-        //test 2 planetsElm.parentNode.removeChild(planetsElm);
-        //test 3 document.getElementById("planets").parentNode.removeChild(document.getElementByID('pluto'));
-    } else if (document.getElementById("pluto").checked === false) {
-        planetsElm.lastChild.style.display="block"
-    }
-
+    } else {
+        planetsElm.lastChild.style.display = "block"
+    };
 }
 
-function clearText(){
+function clearText() {
     outputText.innerHTML = "<br/>";
     document.getElementById("user-weight").value = "";
-
     clicks();
 }
 
 var clickCount = 0;
+
 function clicks() {
     clickCount += 1;
     console.log(clickCount);
-    
+
     if (clickCount === 5) {
-    var tableFlip = document.getElementById("tableFlip").style.visibility="visible";
+        var tableFlip = document.getElementById("tableFlip").style.visibility = "visible";
     } else if (clickCount === 8) {
-        tableFlip = document.getElementById("tableFlip").style.opacity=".4";
+        tableFlip = document.getElementById("tableFlip").style.opacity = ".4";
         document.getElementById("output").innerHTML = "Enjoying yourself?"
     } else if (clickCount === 11) {
-        tableFlip = document.getElementById("tableFlip").style.opacity=".6";
+        tableFlip = document.getElementById("tableFlip").style.opacity = ".6";
     } else if (clickCount === 14) {
-        tableFlip = document.getElementById("tableFlip").style.opacity=".8";$
-    } else if (clickCount >= 20) {
-        tableFlip = document.getElementById("tableFlip").style.opacity="1";
+        tableFlip = document.getElementById("tableFlip").style.opacity = ".8";
+    } else if (clickCount >= 17) {
+        tableFlip = document.getElementById("tableFlip").style.opacity = "1";
         document.getElementById("output").innerHTML = "Refresh the page to start again."
     }
 }
 
-function handleClickEvent(e) {
+function handleClickEvent() {
     var userWeight = document.getElementById("user-weight").value;
     var selectedPlanet = planetsElm.options[planetsElm.selectedIndex].value;
-    // console.log("User's weight is:", userWeight);
-    // console.log("Selected planet is:", selectedPlanet);
-
 
     calculateWeight(userWeight, selectedPlanet);
 }
-
-
 
 calcBtn.addEventListener("click", handleClickEvent);
 checkBox.addEventListener("click", hidePluto);
